3187197fd66242efa4241bb9ec6a65e7dec5c856
JONATAN140502 <jmayangamu@unprg.edu.pe>
2023-06-28 20:21:30 -0500

    cambios  2.0 de clae 2

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -29,7 +29,32 @@ class CategorieController extends Controller
 
         return \response()->json(['state'=>0,'data'=>$categorie],200);
     }
-    
+    protected function show($id){
+        $categorie = \App\Models\Categorie::select('id','name','state')->find($id);
+        return $categorie;
+    }
+
+    //function para editar
+    protected function update(Request $request){
+        
+       
+        $categorie=\App\Models\Categorie::find($request->id);
+        $categorie->name = strtoupper(trim($request->name));
+        $categorie->state=$request->state;
+        $categorie->save();
+        return \response()->json(['state'=>0,'data'=>$categorie],200);
+      
+
+    }
+
+    //function para borrar registro
+    protected function destroy(Request $request){
+        
+        $categorie=\App\Models\Categorie::where('id',$request->id)->delete();
+       
+        return \response()->json(['state'=>0,'id'=>$request->id],200);
+
+    }
 
    
 }
